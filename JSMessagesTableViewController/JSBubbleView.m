3c39cb8242be46f7de4b87688e6ad8356e1d3ae4
Vishal Shah <shah.vish@gmail.com>
2013-04-04 23:36:20 -0400

    Modified the image size to return something

--- JSMessagesTableViewController/JSBubbleView.m
+++ JSMessagesTableViewController/JSBubbleView.m
@@ -133,7 +133,7 @@
     
     CGRect imageFrame = CGRectMake(([self styleIsOutgoing] ? self.frame.size.width - imageSize.width - 20.0f : 20.0f),
                                    bubbleSize.height + kMarginTop,
-                                   imageSize.width,
+                                   (imageSize.width = NAN ? 0.01f: imageSize.width),
                                    imageSize.height);
     self.attachmentView.frame = imageFrame;
 
