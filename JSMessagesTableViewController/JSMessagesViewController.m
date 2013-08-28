cb4f2fce040b74cf1273375852763c2631d2c4ce
bryanreed <breed985@gmail.com>
2013-08-28 18:03:57 -0400

    Remove SDWebImageCache reference (unneeded)

--- JSMessagesTableViewController/JSMessagesViewController.m
+++ JSMessagesTableViewController/JSMessagesViewController.m
@@ -32,7 +32,6 @@
 //  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
 //  OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-#import <SDWebImage/UIImageView+WebCache.h>
 #import <QuartzCore/QuartzCore.h>
 
 #import "JSMessagesViewController.h"
